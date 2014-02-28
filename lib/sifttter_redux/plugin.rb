b83212c4e34c0a0bb3f0b06ab82fc7969b862b3a
Aaron Bach <bachya1208@googlemail.com>
2014-02-28 11:59:33 -0700

    Toying around with plugin idea

--- lib/sifttter_redux/plugin.rb
+++ lib/sifttter_redux/plugin.rb
@@ -5,6 +5,8 @@ module SifttterRedux
   #  Module to easily find the running operating system
   #  ======================================================
   class Plugin
+    attr_accessor :name
+    
     def self.plugins
       @plugins ||= []
     end
@@ -13,9 +15,43 @@ module SifttterRedux
       @plugins ||= []
       @plugins << klass
     end
+
+    def initialize
+      @name = "Unknown Plugin"
+    end
     
-    def self.name
-      puts @name
+    def method_missing(name, *args)
+      raise "#{self.class.name} doesn't implement `#{name}`"
+    end
+    
+    def respond_to_missing?(method_name, include_private)
+      super
     end
   end
 end
+
+# class HelloPlugin < Plugin
+#   def initialize
+#     @name = 'HelloPlugin'
+#   end
+#   
+#   def say_statement
+#     "Hello!"
+#   end
+# end
+# 
+# class GoodbyePlugin < Plugin
+#   def initialize
+#     @name = 'GoodbyePlugin'
+#   end
+#   
+#   def say_statement
+#     "Goodbye!"
+#   end
+# end
+# 
+# h = HelloPlugin.new
+# puts "#{h.class} says: #{h.say_statement}"
+# 
+# g = GoodbyePlugin.new
+# puts "#{g.class} says: #{g.say_statement}"
