1498cb81c5d0fd655c199b9e360881d8c77e1fef
Aaron Bach <bachya1208@googlemail.com>
2014-03-22 12:20:09 -0600

    More cleanup

--- lib/sifttter_redux.rb
+++ lib/sifttter_redux.rb
@@ -17,12 +17,6 @@ module SifttterRedux
   #  Constants
   #  ====================================================
   #  ----------------------------------------------------
-  #  Descriptions
-  #  ----------------------------------------------------
-  DESCRIPTION = 'A customized IFTTT-to-Day One service that allows for smart installation and automated running on a standalone *NIX device (such as a Raspberry Pi).'
-  SUMMARY = 'Automated IFTTT to Day One engine.'
-
-  #  ----------------------------------------------------
   #  Dropbox Uploader
   #  ----------------------------------------------------
   DBU_CONFIG_FILEPATH = File.join(ENV['HOME'], '.dropbox_uploader')
