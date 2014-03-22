a72f1dd7af9a7de9b3c45f6603f47540edd3c9fa
Aaron Bach <bachya1208@googlemail.com>
2014-03-22 08:42:46 -0600

    Documentation added to Configuration module

--- lib/sifttter_redux/configuration.rb
+++ lib/sifttter_redux/configuration.rb
@@ -27,23 +27,60 @@ module SifttterRedux
         Hash === v1 && Hash === v2 ? v1.merge(v2, &merger) : v2 }
       target.merge! data, &merger
     end
-    
+
+    #  ----------------------------------------------------
+    #  method_missing method
+    #
+    #  Allows this module to return data from the config
+    #  Hash when given a method name that matches a key.
+    #  @param name
+    #  @param *args
+    #  @param &block
+    #  @return Hash
+    #  ----------------------------------------------------    
     def method_missing(name, *args, &block)
       @_data[name.to_sym] || @_data.merge!(name.to_sym => {})
     end
 
+    #  ----------------------------------------------------
+    #  add_section method
+    #
+    #  Adds a new section to the config file (if it doesn't
+    #  already exist).
+    #  @param section_name The section to add
+    #  @return Void
+    #  ----------------------------------------------------
     def self.add_section(section_name)
       @_data[section_name] = {} unless @_data.key?(section_name)
     end
-    
+
+    #  ----------------------------------------------------
+    #  config_path method
+    #
+    #  Returns the path to the configuration file
+    #  @return String
+    #  ----------------------------------------------------
     def self.config_path
       @_config_path
     end
-    
+
+    #  ----------------------------------------------------
+    #  delete_section method
+    #
+    #  Removes a section to the config file (if it exists).
+    #  @param section_name The section to remove
+    #  @return Void
+    #  ----------------------------------------------------
     def self.delete_section(section_name)
       @_data.delete(section_name) if @_data.key?(section_name)
     end
-    
+
+    #  ----------------------------------------------------
+    #  dump method
+    #
+    #  Returns the config Hash.
+    #  @return Hash
+    #  ----------------------------------------------------    
     def self.dump
       @_data
     end
